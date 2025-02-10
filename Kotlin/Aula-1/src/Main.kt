dff0559f94c97424f8161990a0e409d6ccfb9885
Lucas Henrique de Sousa <163554117+LucasHenriqueSousa@users.noreply.github.com>
2025-02-10 09:03:17 -0300

    Update Main.kt

--- Kotlin/Aula-1/src/Main.kt
+++ Kotlin/Aula-1/src/Main.kt
@@ -1,5 +1,9 @@
+//Fun main - Função principal
 fun main() {
+    //   print() - sem quebra de linha
+    //  * Pode se usar o "\n" para querbar a linha no comanda print.
     print("Lucas\n")
+    //println - com quabra de linha passado no comando.
     println("Henrique")
     println("Sousa")
     println("Chapter")
