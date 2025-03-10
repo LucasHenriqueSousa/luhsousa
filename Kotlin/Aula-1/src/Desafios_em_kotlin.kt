0caa84a237036c452748f05ff6ccc3db647cef3a
Lucas Henrique de Sousa <163554117+LucasHenriqueSousa@users.noreply.github.com>
2025-03-10 11:24:51 -0300

    Desafios em kotlin
    Para entregar complemento da prova.

---
+++ Kotlin/Aula-1/src/Desafios_em_kotlin.kt
@@ -0,0 +1,27 @@
+[DESAFIO] Escreva um programa para calcular a redução do tempo de vida de um fumante. Pergunte a quantidade de cigarros fumados por dias e quantos anos ele já fumou. Considere que um fumante perde 10 min de vida a cada cigarro. Calcule quantos dias de vida um fumante perderá e exiba o total em dias.
+[DESAFIO] Crie um programa que leia o tamanho de três segmentos de reta. Analise seus comprimentos e diga se é possível formar um triângulo com essas retas. Matematicamente, para três segmentos formarem um triângulo, o comprimento de cada lado deve ser menor que a soma dos outros dois.
+[DESAFIO] Refaça o algoritmo 25, acrescentando o recurso de mostrar que tipo de triângulo será formado:
+EQUILÁTERO: todos os lados iguais
+ISÓSCELES: dois lados iguais
+ESCALENO: todos os lados diferentes
+
+[DESAFIO] Crie um jogo de JoKenPo (Pedra-Papel-Tesoura)
+
+[DESAFIO] Crie um jogo onde o computador vai sortear um número entre 1 e 5 o jogador vai tentar descobrir qual foi o valor sorteado.
+
+[DESAFIO] Vamos melhorar o jogo que fizemos no exercício 32. A partir de agora, o computador vai sortear um número entre 1 e 10 e o jogador vai ter 4 tentativas para tentar acertar.
+
+[DESAFIO] Desenvolva um programa que leia o primeiro termo e a razão de uma PA (Progressão Aritmética), mostrando na tela os 10 primeiros elementos da PA e a soma entre todos os valores da sequência.
+[DESAFIO] Faça um programa que mostre os 10 primeiros elementos da Sequência de Fibonacci:
+1 1 2 3 5 8 13 21...
+
+
+
+[DESAFIO] Crie uma lógica que preencha um vetor de 20 posições com números aleatórios (entre 0 e 99) gerados pelo computador. Logo em seguida, mostre os números gerados e depois coloque o vetor em ordem crescente, mostrando no final os valores ordenados.
+
+
+[DESAFIO] Desenvolva um aplicativo que tenha um procedimento chamado Fibonacci() que recebe um único valor inteiro como parâmetro, indicando quantos termos da sequência serão mostrados na tela. O seu procedimento deve receber esse valor e mostrar a quantidade de elementos solicitados.
+Obs: Use os exercícios 70 e 75 para te ajudar na solução Ex:
+Fibonacci(5) vai gerar 1 >> 1 >> 2 >> 3 >> 5 >> FIM
+Fibonacci(9) vai gerar 1 >> 1 >> 2 >> 3 >> 5 >> 8 >> 13 >> 21 >> 34 >> FIM
+
